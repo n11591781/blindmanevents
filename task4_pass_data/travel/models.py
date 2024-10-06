1196779d72748f5e4f2754137ae4e1223278dd0d
n11591781 <n11591781@qut.edu.au>
2024-10-06 14:17:49 +1000

    Changed Models.py

--- task4_pass_data/travel/models.py
+++ task4_pass_data/travel/models.py
@@ -3,44 +3,52 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users' # good practice to specify table name
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    username = db.Column(db.String(100), index=True, unique=True, nullable=False)
     emailid = db.Column(db.String(100), index=True, nullable=False)
-	# password should never stored in the DB, an encrypted password is stored
-	# the storage should be at least 255 chars long, depending on your hashing algorithm
     password_hash = db.Column(db.String(255), nullable=False)
-    # relation to call user.comments and comment.created_by
     comments = db.relationship('Comment', backref='user')
+    booking_history = db.relationship('PurchasedTicket', backref='user')
     
-    # string print method
     def __repr__(self):
-        return f"Name: {self.name}"
+        return f"Name: {self.username}"
 
-class Destination(db.Model):
-    __tablename__ = 'destinations'
+class Event(db.Model):
+    __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    description = db.Column(db.String(200))
+    title = db.Column(db.String(80))
+    date = db.Column(db.String(80))
+    time = db.Column(db.String(80))
+    location = db.Column(db.String(100))
+    event_description = db.Column(db.String(200))
+    category = db.Column(db.String(20))
     image = db.Column(db.String(400))
-    currency = db.Column(db.String(3))
-    # ... Create the Comments db.relationship
-	# relation to call destination.comments and comment.destination
-    comments = db.relationship('Comment', backref='destination')
+    tickets_remaining = db.Column(db.Integer)
+    comments = db.relationship('Comment', backref='event')
+    booking_history = db.relationship('PurchasedTicket', backref='event')
 	
-    # string print method
     def __repr__(self):
-        return f"Name: {self.name}"
+        return f"Name: {self.title}"
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(400))
     created_at = db.Column(db.DateTime, default=datetime.now())
-    # add the foreign key
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
-    # string print method
     def __repr__(self):
         return f"Comment: {self.text}"
+    
+class PurchasedTicket(db.Model, UserMixin):
+    __tablename__ = 'purchased_tickets'
+    id = db.Column(db.Integer, primary_key=True)
+    ticket_id = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    booked_on = db.Column(db.DateTime, default=datetime.now())
+    
+    def __repr__(self):
+        return f"Name: {self.username}"
