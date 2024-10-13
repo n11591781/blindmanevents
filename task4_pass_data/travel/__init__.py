be8cd9cfb1b4f36f210216445e8b8394157c7372
Andrew Phan <andrewphan@Andrews-MacBook-Pro.local>
2024-10-13 10:50:57 +1000

    Added 404 and 500 error handling and tested error routes

--- task4_pass_data/travel/__init__.py
+++ task4_pass_data/travel/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -10,21 +9,21 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    #we use this utility module to display forms quickly
+    # we use this utility module to display forms quickly
     Bootstrap5(app)
 
-    #this is a much safer way to store passwords
+    # this is a much safer way to store passwords
     Bcrypt(app)
 
-    #a secret key for the session object
-    #(it would be better to use an environment variable here)
+    # a secret key for the session object
+    # (it would be better to use an environment variable here)
     app.secret_key = 'somerandomvalue'
 
-    #Configue and initialise DB
+    # Configure and initialise DB
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bmdb.sqlite'
     db.init_app(app)
 
-    #config upload folder
+    # config upload folder
     UPLOAD_FOLDER = '/static/image'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
     
@@ -33,11 +32,11 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    # create a user loader function takes userid and returns User
+    # create a user loader function that takes user_id and returns User
     from .models import User  # importing here to avoid circular references
     @login_manager.user_loader
     def load_user(user_id):
-        return db.session.scalar(db.select(User).where(User.id==user_id))
+        return db.session.scalar(db.select(User).where(User.id == user_id))
 
     # add Blueprints
     from . import views
@@ -47,16 +46,21 @@ def create_app():
     from . import auth
     app.register_blueprint(auth.authbp)
 
-    @app.errorhandler(404) 
-    # inbuilt function which takes error as parameter 
-    def not_found(e): 
-      return render_template("404.html", error=e)
+    # Error handling for 404 and 500 errors
+    @app.errorhandler(404)
+    def not_found(e):
+        # 404 Page Not Found
+        return render_template("404.html", error=e), 404
 
-    # this creates a dictionary of variables that are available
-    # to all html templates
+    @app.errorhandler(500)
+    def internal_error(e):
+        # 500 Internal Server Error
+        return render_template("500.html", error=e), 500
+
+    # Context processor to pass the current year to all templates
     @app.context_processor
     def get_context():
-      year = datetime.datetime.today().year
-      return dict(year=year)
+        year = datetime.datetime.today().year
+        return dict(year=year)
 
     return app
